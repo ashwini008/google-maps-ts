3f23af2c3de29c146a06543799cb8e2e0293cff9
Ashwini Ashish <ashwini.ashish8@gmail.com>
2022-03-31 23:38:51 +0530

    Fix lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.VFC = () => {
 
     const [markers, setMarkers] = React.useState<google.maps.LatLng[]>([]);
     const [zoom, setZoom] = React.useState(18); // initial zoom
-    const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({ });
+    const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({ lat: -25.344, lng: 131.036 });
 
     useEffect(() => {
         // HTML5 geolocation.
@@ -26,6 +26,7 @@ const App: React.VFC = () => {
 
                 
                 setCenter(pos);
+                // @ts-ignore
                 setMarkers([pos]);
             },
             () => {
@@ -38,12 +39,71 @@ const App: React.VFC = () => {
         }
     }, []);
 
+    const onClick = (e: google.maps.MapMouseEvent) => {
+        // avoid directly mutating state
+        setMarkers([...markers, e.latLng!]);
+    };
+
+    const onIdle = (m: google.maps.Map) => {
+        console.log("onIdle");
+        setZoom(m.getZoom()!);
+        setCenter(m.getCenter()!.toJSON());
+    };
+
+    const form = (
+        <div
+            style={{
+                padding: "1rem",
+                flexBasis: "250px",
+                height: "100%",
+                overflow: "auto",
+            }}
+        >
+            {/* <label htmlFor="zoom">Zoom</label>
+            <input
+                type="number"
+                id="zoom"
+                name="zoom"
+                value={zoom}
+                onChange={(event) => setZoom(Number(event.target.value))}
+            />
+            <br />
+            <label htmlFor="lat">Latitude</label>
+            <input
+                type="number"
+                id="lat"
+                name="lat"
+                value={center.lat}
+                onChange={(event) =>
+                    setCenter({ ...center, lat: Number(event.target.value) })
+                }
+            />
+            <br />
+            <label htmlFor="lng">Longitude</label>
+            <input
+                type="number"
+                id="lng"
+                name="lng"
+                value={center.lng}
+                onChange={(event) =>
+                    setCenter({ ...center, lng: Number(event.target.value) })
+                }
+            />
+            <h3>{markers.length === 0 ? "Click on map to add markers" : "Markers"}</h3>
+            {markers.map((latLng, i) => (
+                <pre key={i}>{JSON.stringify(latLng.toJSON(), null, 2)}</pre>
+            ))}
+            <button onClick={() => setMarkers([])}>Clear</button> */}
+        </div>
+    );
 
     return (
         <div style={{ display: "flex", height: "100%" }}>
             <Wrapper apiKey={"AIzaSyDaJ7GjeeOpguv0KPT97d4BqYECBo2qkAM"} render={render}>
                 <Map
                     center={center}
+                    // onClick={onClick}
+                    // onIdle={onIdle}
                     zoom={zoom}
                     style={{ flexGrow: "1", height: "100%" }}
                 >
@@ -52,6 +112,8 @@ const App: React.VFC = () => {
                     ))}
                 </Map>
             </Wrapper>
+            {/* Basic form for controlling center and zoom of map. */}
+            {/* {form} */}
         </div>
     );
 };
